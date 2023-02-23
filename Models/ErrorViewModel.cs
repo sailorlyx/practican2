01bb983813128eb86c9f29178fceb436273853ab
Lili <lidiaechuacas@hotmail.com>
2023-02-23 11:54:48 +0100

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practican2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
